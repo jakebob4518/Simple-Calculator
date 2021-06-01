ba08d986bfaa252509ee59a4ca058e5aeb341789
jakebob4518 <74426060+jakebob4518@users.noreply.github.com>
2021-06-01 19:16:54 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,27 +1,38 @@
 def add(x, y):
     return x + y
+
 def sub(x, y):
     return x - y
+
 def mul(x, y):
     return x * y
+
 def div(x, y):
     return x / y
-print('This is a simple calculator: ')
-print('1. Addition')
-print('2. Subtraction')
-print('3. Multiplication')
-print('4. Division') 
+
+print('''
+    This is a simple calculator:
+    1. Addition
+    2. Subtraction
+    3. Multiplication
+    4 Division
+''')
+
 x = 1
 while x == 1:    
     choice = input('Choose from the options(1. 2. 3. 4.): ')
     num1 = int(input('Enter first number: '))
     num2 = int(input('Enter second number: '))
+    
     if choice == '1':
         print(f'{num1} + {num2} = {add(num1, num2)}')
+    
     if choice == '2':
         print(f'{num1} - {num2} = {sub(num1, num2)}')
+    
     if choice == '3':
         print(f'{num1} * {num2} = {mul(num1, num2)}')
+    
     if choice == '4':
         print(f'{num1} / {num2} = {div(num1, num2)}')
         
